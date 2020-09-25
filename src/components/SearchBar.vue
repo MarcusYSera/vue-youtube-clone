82109959ad2018a97437b49e081636d1edb339ba
Marcus Sera <m.yoshiosera@gmail.com>
2020-09-24 17:38:54 -0700

    working on video displayer sourced from youtube

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Search Bar
+    <input @input="onInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  methods: {
+    onInput: event => {
+      console.log(event.target.value)
+    },
+  },
+}
+</script>
+
+<style></style>
